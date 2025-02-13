03d91fad2973154607f9a6083b25c698371820b3
neilfabiao <50138381+NeilFabiao@users.noreply.github.com>
2025-02-13 21:28:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_title="Telco Churn - Entrevista Vodacom",
     page_icon="📊",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="collapsed"
 )
 
 # ----------------------------------------------------
