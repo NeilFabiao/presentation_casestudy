1dfceea53a4132ce583fb65c7b490672a308b3be
neilfabiao <50138381+NeilFabiao@users.noreply.github.com>
2025-02-13 21:31:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -104,10 +104,9 @@ st.title("Telco Churn Analysis 📊")
 
 st.write(
     "Bem-vindo ao **Dashboard de Análise Churn (Rotatividade) de Telecomunicações!** 🚀 "
-    
- "Este relatório interativo explora importantes insights sobre a rotatividade de clientes, ajudando-nos"
- " compreender as tendências, desafios e estratégias para melhorar a retenção."
-)
+    Este relatório interativo explora insights importantes sobre a rotatividade de clientes, 
+    ajudando-nos a compreender as tendências, desafios e estratégias para melhorar a retenção. 
+    (Para filtrar os dados, clique na opção à esquerda.)")
 
 st.markdown(
     "### 🔍 Principais descobertas:\n"
